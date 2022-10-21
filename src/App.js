4bb1b190aec32deee569af899569466e2be63b5c
tnsilva <tnsilva175@gmail.com>
2022-10-21 17:44:54 -0300

    feat: useEffect initial load

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
   const [repositories, setRepositories] = useState([]);
 
-  function handleAddRepository() {
-    setRepositories([
-      ...repositories,
-      { id: Math.random(), name: "Novo repo" },
-    ]);
-  }
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://api.github.com/users/tnsilva/repos"
+      );
+      const data = await response.json();
+      console.log({ data });
+      setRepositories(data);
+    }
+
+    fetchData();
+  }, []);
 
   return (
-    <>
-      <ul>
-        {repositories.map((repo) => (
-          <li key={repo.id}>{repo.name}</li>
-        ))}
-      </ul>
-      <button onClick={handleAddRepository}>Adicionar repositório</button>
-    </>
+    <ul>
+      {repositories.map((repo) => (
+        <li key={repo.id}>{repo.name}</li>
+      ))}
+    </ul>
   );
 }
