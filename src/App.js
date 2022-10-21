268d51e9bb1802bd6fdd578563b1043190f7cbd3
tnsilva <tnsilva175@gmail.com>
2022-10-21 17:32:47 -0300

    feat: useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  function handleAddRepository() {
+    setRepositories([
+      ...repositories,
+      { id: Math.random(), name: "Novo repo" },
+    ]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        {repositories.map((repo) => (
+          <li key={repo.id}>{repo.name}</li>
+        ))}
+      </ul>
+      <button onClick={handleAddRepository}>Adicionar repositório</button>
+    </>
   );
 }
-
-export default App;
