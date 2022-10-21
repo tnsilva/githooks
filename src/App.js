8773fa9aa35f25af7a31b0720597f6da19247884
tnsilva <tnsilva175@gmail.com>
2022-10-21 18:03:51 -0300

    feat: favorite repo

--- src/App.js
+++ src/App.js
@@ -16,10 +16,22 @@ export default function App() {
     fetchData();
   }, []);
 
+  function handleFavorite(id) {
+    const newRepositories = repositories.map((repo) => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
+    });
+
+    setRepositories(newRepositories);
+  }
+
   return (
     <ul>
       {repositories.map((repo) => (
-        <li key={repo.id}>{repo.name}</li>
+        <li key={repo.id}>
+          {repo.name}
+          {repo.favorite && <span>(Favorito)</span>}
+          <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+        </li>
       ))}
     </ul>
   );
